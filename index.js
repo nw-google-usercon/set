23e2f69fcd74a89e20ae306109a00e4f88a6c29b
nw-google-usercon <oldgamesgl@outlook.com>
2024-09-27 14:43:27 +0300

    ss

---
+++ index.js
@@ -0,0 +1,14 @@
+window.screenOrientation = "sensor_landscape", 
+//loadLib("./libs/laya.core.js"),
+//  loadLib("./libs/laya.ani.js"),
+//   loadLib("./libs/laya.ui.js"),
+//    loadLib("./libs/laya.d3.js"), 
+//    loadLib("./libs/laya.physics3D.js"),
+
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.7.3/laya.core.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.7.3/laya.ani.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.7.3/laya.ui.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.7.3/laya.d3.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.7.3/laya.physics3D.js"),
+   //loadLib("./AdSdk/Sdk.js"),
+    loadLib("./js/main.js");                                                                                                 
